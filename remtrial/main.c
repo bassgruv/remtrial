608fd52f67267a250e94c7c86380fa6e9d41c0a7
bassgruv <bassgruv@mac.com>
2017-05-30 07:56:29 -0700

    Initial Commit

---
+++ remtrial/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  remtrial
+//
+//  Created by Lantsberger, Mark on 5/30/17.
+//  Copyright © 2017 Lantsberger, Mark. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
